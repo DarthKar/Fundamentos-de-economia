44ad2b6fd2601fd9117a05cf0fc9b0bf6b4c6ecc
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2025-02-18 17:24:39 -0500

    Update economia.py

--- economia.py
+++ economia.py
@@ -6,7 +6,7 @@ data['Fecha'] = pd.to_datetime(data['Fecha'], format='%Y/%m/%d')
 data['Growth Rate'] = data['TRM'].pct_change() * 100
 plt.figure(figsize=(14, 7))
 plt.subplot(2, 1, 1)
-plt.plot(data['Fecha'], data['TRM'], label='TRM', color='blue')
+plt.plot(data['Fecha'], data['TRM'], label='TRM', color='green')
 plt.title('Tasa Representativa del Mercado (TRM)')
 plt.xlabel('Fecha')
 plt.ylabel('TRM')
@@ -23,12 +23,12 @@ plt.show()
 def calculoDias(data):
     max_growth_day = data[data['Growth Rate'] == data['Growth Rate'].max()]
     min_growth_day = data[data['Growth Rate'] == data['Growth Rate'].min()]
-    print("Mayor Crecimiento:")
+    mayor_precio = data[data['TRM'] == data['TRM'].max()]
+    print("\nMayor Crecimiento:")
     print(max_growth_day[['Fecha', 'TRM', 'Growth Rate']])
     print("\nMenor Crecimiento:")
     print(min_growth_day[['Fecha', 'TRM', 'Growth Rate']])
-
-
-    print('mayor precio')
+    print('\nmayor precio')
+    print(mayor_precio)
     
 calculoDias(data)
